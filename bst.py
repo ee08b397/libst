b18f127611fc9f600f44fcfc4d309d945d6ee250
Dewei <deweichen47@gmail.com>
2015-01-19 09:40:18 -0500

    added level print

--- bst.py
+++ bst.py
@@ -7,6 +7,8 @@ class BST(object):
             self.left = None
             self.right = None
             self.data = val
+            self.x = 0
+            self.y = 0
 
 
     def __init__(self):
@@ -14,7 +16,6 @@ class BST(object):
 
 
     def _insert(self, root, val):
-        """Insert a node under root"""
         if root == None:
             root = self.Node(val)
         else:
@@ -32,27 +33,69 @@ class BST(object):
 
 
     def _print_tree(self, root, order):
-        """Print tree nodes with in/pre/post-order traversal"""
         if root == None:
             return
         else:
             if order == "INORDER":
                 self._print_tree(root.left, order)
-                print root.data
+                print root.data,
                 self._print_tree(root.right, order)
             elif order == "PREORDER":
-                print root.data
+                print root.data,
                 self._print_tree(root.left, order)
                 self._print_tree(root.right, order)
             elif order == "POSTORDER":
                 self._print_tree(root.left, order)
                 self._print_tree(root.right, order)
-                print root.data
+                print root.data,
 
 
     def print_tree(self, order="INORDER"):
-        self._print_tree(self.root, order)
+        """Print tree nodes with in/pre/post-order traversal"""
+        if order == "INORDER" or order == "PREORDER" or order == "POSTORDER":
+            print order + ": ",
+            self._print_tree(self.root, order)
+            print
 
+
+    def print_level(self):
+        if not self.root:
+            return            
+        queue = []
+        queue.append(self.root)
+        counter = 1
+        while len(queue) > 0:
+            temp = counter
+            counter = 0
+            for i in range(temp):
+                node = queue.pop(0)
+                print str(node.data),
+                if node.left:
+                    queue.append(node.left)
+                    counter += 1
+                if node.right:
+                    queue.append(node.right)
+                    counter += 1
+            print   # Go to next level
+        
+
+    def _knuth_layout(self, root, depth):
+        if root.left: 
+            self._knuth_layout(root.left, depth + 1)
+        root.x = self.i
+        root.y = depth
+        self.i += 1 
+        if root.right:
+            self._knuth_layout(root.right, depth + 1)
+
+
+    def print_knuth(self):
+        if self.root == None:   
+            return 
+        # Generate the layouts
+        self.i = 0
+        self._knuth_layout(self.root)
+            
 
     def _max_height(self, root):
         if root == None:
@@ -89,9 +132,11 @@ if __name__ == "__main__":
     bst.insert(13)
 
     bst.print_tree("INORDER")
+    bst.print_level()
 
     if bst.is_balanced():
         print "Tree is balanced"    
     else:
         print "Tree is not balanced"
+
 
