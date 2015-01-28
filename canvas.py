f7f933d721d6110aab78badaf9b6ef5e6b1db0d8
Dewei <deweichen47@gmail.com>
2015-01-27 23:41:02 -0500

    done with shift

--- canvas.py
+++ canvas.py
@@ -15,11 +15,31 @@ class Canvas(object):
             self._y = 0
             self._mat = [[None] * self._MAX_X for i in range(self._MAX_Y)]
 
+
+        def _shift(self, axis, s):
+            lim = self._MAX_X if axis == "X" else self._MAX_Y
+            if s > 0 and self._x + self._wd + s > lim:
+                self._x = self._MAX_X - self._wd
+            elif s < 0 and self._x + s < 0:
+                self._x = 0
+            else:
+                self._x += s
+
+
+        def _shift_y(self, s):
+            if s > 0 and self._y + self._ht + s > self._MAX_Y:
+                self._x = self._MAX_X - self._wd
+            elif s < 0 and self._x + s < 0:
+                self._x = 0
+            else:
+                self._x += s
+
         
         def _draw_frame(self): 
             sys.stdout.write("".join(["-"] * (self._wd + 2 )) + "\n")
             for row in self._mat[self._y:(self._y+self._ht)]: 
-                sys.stdout.write("|" + "".join(row[self._y:(self._y+self.width)]) + "|\n")
+                line = map(lambda x : " " if x == None else x, row[self._x:(self._x+self._wd)])
+                sys.stdout.write("|" + "".join(line) + "|\n")
             sys.stdout.write("".join(["-"] * (self._wd + 2 )) + "\n")
 
 
@@ -38,7 +58,7 @@ class Canvas(object):
     
 
     def __init__(self, frame_width, frame_height):
-        self.space = Space(frame_width, frame_height)
+        self.space = self.Space(frame_width, frame_height)
 
 
     def print_int(self, x, y, val):
@@ -48,18 +68,28 @@ class Canvas(object):
             self.space._putc(x, y, c)
             x += 1
 
+
     def print_str(self, x, y, val):
         if type(val) is not str:
             return
         for c in val:
             self.space._putc(x, y, c) 
             x += 1
+
+    
+    def move_x(self, s):
+        self.space._shift_x(s)
+
+
+    def draw(self):
+        self.space._draw_frame()
         
 
 if __name__ == "__main__":
     
     canvas = Canvas(80, 50)
-    canvas.print_int(1, 1, 1234)
+    canvas.print_int(0, 0, 1234)
     canvas.print_str(4, 4, "hello")
-    canvas.draw_frame()
+    canvas.move_x(2)
+    canvas.draw()
 
